a6e196c8774c2dbad8f445ae3190b2fa18e70163
kanakanho <kahoshibakichi@gmail.com>
2025-07-16 16:37:15 +0900

    Initial Commit

---
+++ VirtualProduction/VirtualProductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualProductionApp.swift
+//  VirtualProduction
+//
+//  Created by blueken on 2025/07/16.
+//
+
+import SwiftUI
+
+@main
+struct VirtualProductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VirtualProductionTests/VirtualProductionTests.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualProductionTests.swift
+//  VirtualProductionTests
+//
+//  Created by blueken on 2025/07/16.
+//
+
+import Testing
+@testable import VirtualProduction
+
+struct VirtualProductionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
